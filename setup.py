3bb9a8c986a47de44915928573dd5b61fdad3a53
Alejandro <49617228+fariascl@users.noreply.github.com>
2020-10-01 17:55:12 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pyemailscraper',
   packages = ['pyemailscraper'],
-  version = '0.1.4',
-  description = 'A library for scrape all mail address in a website',
+  version = '0.1.5',
+  description = 'A library for scrape all email address in a website',
   author = 'Alejandro Farías',
   author_email = 'farias@8loop.cl',
   url = 'https://github.com/fariascl/pyemailscraper', # use the URL to the github repo
-  download_url = 'https://github.com/fariascl/pyemailscaper/tarball/0.1.4',
-  keywords = ['scraper', 'mail', 'websites'],
+  download_url = 'https://github.com/fariascl/pyemailscaper/tarball/0.1.5',
+  keywords = ['scraper', 'email', 'websites'],
   classifiers = [],
 )

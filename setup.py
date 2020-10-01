3495fd81b4c49a51821704f3e67bbb74330f62bd
Alejandro <49617228+fariascl@users.noreply.github.com>
2020-10-01 16:08:27 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'A library for scrape all mail address in a website',
   author = 'Alejandro Farías',
   author_email = 'farias@8loop.cl',
-  url = 'https://github.com/fariascl/pymailscraper', # use the URL to the github repo
-  download_url = 'https://github.com/fariascl/pymailscaper/tarball/0.1',
+  url = 'https://github.com/fariascl/pyemailscraper', # use the URL to the github repo
+  download_url = 'https://github.com/fariascl/pyemailscaper/tarball/0.1',
   keywords = ['scraper', 'mail', 'websites'],
   classifiers = [],
 )
